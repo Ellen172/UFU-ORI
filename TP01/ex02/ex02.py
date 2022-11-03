9515d0d9ed8a426a0bb99ef517cdf04e6f5b312e
Ellen172 <ellencas172@gmail.com>
2022-11-03 20:09:44 -0300

    tp1 feito em casa

--- TP01/ex02/ex02.py
+++ TP01/ex02/ex02.py
@@ -3,14 +3,18 @@ import re
 
 entrada_vocabulario = open('/home/ellen/Documentos/UFU-ORI/TP01/ex02/vocabulario.txt', 'r')
 vocabulario = entrada_vocabulario.read().split()
-print('vocabulario: ')
-print(vocabulario)
 
 entrada_texto = open('/home/ellen/Documentos/UFU-ORI/TP01/ex02/texto.txt')
 texto_lido = entrada_texto.read()
 texto = re.sub(r'[^\w\s]','', unidecode(texto_lido)).lower().split()
-print('texto: ')
-print(texto)
+
+freq = []
+for termo in vocabulario: 
+    if(termo in texto): 
+        freq.append(1)
+    else:
+        freq.append(0)
+print(freq)
 
 entrada_vocabulario.close()
 entrada_texto.close()
